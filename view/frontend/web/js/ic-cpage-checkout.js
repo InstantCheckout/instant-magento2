d8e28a2be2d7c3596a45df4746da0379673c5d47
William Gao <will@instant.one>
2023-02-22 10:49:36 +0000

    Ffurther changes

--- view/frontend/web/js/ic-cpage-checkout.js
+++ view/frontend/web/js/ic-cpage-checkout.js
@@ -11,60 +11,6 @@ define([
 
         initialize: function () {
             this._super();
-
-            const verificationElementEnabled = checkoutHelper.getInstantPayParams().bannerElement.theme;
-            const verificationElementEmailFieldSelector = checkoutHelper.getInstantPayParams().verificationElement.emailFieldSelector;
-            
-            const bannerElementEnabled = checkoutHelper.getInstantPayParams().bannerElement.theme;
-            const bannerElementTargetElementSelector = checkoutHelper.getInstantPayParams().bannerElement.targetElementSelector;
-            const bannerElementShowAfterElement = checkoutHelper.getInstantPayParams().bannerElement.shouldAppendToElement;
-            const bannerElementTheme = checkoutHelper.getInstantPayParams().bannerElement.theme;
-
-            let verificationElementLoadInterval;
-            let bannerElementLoadInterval;
-            if (verificationElementEnabled){
-                const verificationElementLoadInterval = setInterval(() => {
-                    if (window.InstantJS && (document.querySelector(verificationElementEmailFieldSelector) || window.checkoutConfig?.customerData?.email)) {
-                        clearInterval(verificationElementLoadInterval);
-    
-                        window.InstantJS.createVerificationElement(
-                            verificationElementEmailFieldSelector,
-                            {
-                                merchantId: checkoutHelper.getInstantPayParams().merchantId,
-                                storeCode: checkoutHelper.getInstantPayParams().storeCode,
-                                cartId: checkoutHelper.getInstantPayParams().cartId
-                            }
-                        );
-                    }
-                }, 50);
-            }
-
-            if (bannerElementEnabled){
-                const bannerElementLoadInterval = setInterval(() => {
-                    if (window.InstantJS && document.querySelector(bannerElementTargetElementSelector)) {
-                        clearInterval(bannerElementLoadInterval);
-    
-                        window.InstantJS.createInstantPayBannerElement(
-                            bannerElementTargetElementSelector,
-                            window.checkoutConfig?.customerData?.email ?? '',
-                            window.checkoutConfig?.customerData?.firstname ?? '',
-                            bannerElementShowAfterElement,
-                            bannerElementTheme
-                        );
-                    }
-                }, 50);
-            }
-
-            // Clear the intervals if we don't find either dom element after 30 seconds.
-            setTimeout(() => {
-                if (verificationElementLoadInterval) {
-                    clearInterval(verificationElementLoadInterval);
-                }
-                if (bannerElementLoadInterval) {
-                    clearInterval(bannerElementLoadInterval);
-                }
-            }, 30000)
-
             return this;
         },
 
