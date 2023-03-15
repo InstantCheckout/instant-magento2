82b3b76c531298844d41aaf6964a62f9073de279
James Officer <jofficer@instant.one>
2023-03-15 01:25:54 +0000

    Use  to get store name

--- Controller/Adminhtml/Activation/Send.php
+++ Controller/Adminhtml/Activation/Send.php
@@ -128,16 +128,12 @@ class Send extends Action
         return $this->scopeConfig->getValue('trans_email/ident_support/email', \Magento\Store\Model\ScopeInterface::SCOPE_STORE);
     }
 
-    private function getStoreName(): string
-    {
-        return $this->scopeConfig->getValue('general/store_information/name', \Magento\Store\Model\ScopeInterface::SCOPE_STORE);
-    }
-
     private function getAppIdAndTokenFromBackend()
     {
         $instantIntegration = $this->integrationFactory->create()->load(static::INTEGRATION_NAME, 'name')->getData();
         $consumer = $this->oauthService->loadConsumer($instantIntegration["consumer_id"]);
         $token = $this->oauthToken->loadByConsumerIdAndUserType($consumer->getId(), 1);
+        $store = $this->storeManagerInterface->getStore();
 
         $postData = [
             'consumerKey'       => $consumer->getKey(),
@@ -145,8 +141,8 @@ class Send extends Action
             'accessToken'       => $token->getToken(),
             'accessTokenSecret' => $token->getSecret(),
             'platform'          => 'MAGENTO',
-            'baseUrl'           => $this->storeManagerInterface->getStore()->getBaseUrl(),
-            'merchantName'      => $this->getStoreName(),
+            'baseUrl'           => $store->getBaseUrl(),
+            'merchantName'      => $store->getName(),
             'email'             => $this->getStoreEmail(),
             'isStaging'         => true,
         ];
