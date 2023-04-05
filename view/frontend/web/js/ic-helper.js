cde54b25629ad392c718b26064ef6cb90082ec2a
James Officer <jofficer@instant.one>
2023-04-05 02:25:29 +0000

    ,.

--- view/frontend/web/js/ic-helper.js
+++ view/frontend/web/js/ic-helper.js
@@ -51,12 +51,14 @@ define([
                         address: data.address,
                     };
 
-                    const setSessionIdInterval = setInterval(() => {
-                        if (window.InstantM2) {
-                            window.InstantM2.sessionId = data.sessionId;
-                            clearInterval(setSessionIdInterval);
-                        }
-                    }, 100)
+                    if (data.sessionId) {
+                        const setSessionIdInterval = setInterval(() => {
+                            if (window.InstantM2) {
+                                window.InstantM2.sessionId = data.sessionId;
+                                clearInterval(setSessionIdInterval)
+                            }
+                        }, 100);
+                    }
 
                     $(document).trigger('instant-config-loaded');
                     if (typeof callback === 'function') {
